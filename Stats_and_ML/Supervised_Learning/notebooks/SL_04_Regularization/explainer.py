0fbc96bb3e0dc243d604ad290c693bc3157db34a
Michael Holloway <mcvholloway@gmail.com>
2022-03-19 14:48:34 -0500

    Update regularization notebook with scaler

--- Stats_and_ML/Supervised_Learning/notebooks/SL_04_Regularization/explainer.py
+++ Stats_and_ML/Supervised_Learning/notebooks/SL_04_Regularization/explainer.py
@@ -60,3 +60,57 @@ def tell_me_why(linreg, sample, column_names):
                 color = 'red')
 
     plt.axis('off');
+    
+    
+    
+def tell_me_why_pipe(linreg, sample, column_names):
+    '''
+    This will generate a plot showing why a linear regression model made the predictions that it did.
+    It currently only works specifically for the King County housing data.
+    
+    parameters:
+    	linreg: A (fit) linear regression model.
+    	sample: A single instance for which you want an explanation of linreg's prediction.
+    	column_names: The predictor column names in the correct order
+    '''
+
+    plt.figure(figsize = (14, 10))
+
+    nonzip = [colname for colname in column_names if 'zipcode' not in colname]
+    zips = [colname for colname in column_names if 'zipcode' in colname]
+
+    plt.ylim(0, 21)
+    plt.xlim(-1, 3)
+
+    fontsize = 18
+
+    plt.annotate(xy = (1.5, 20), text = 'coefficient', fontsize = fontsize, ha = 'right', fontweight = 'bold')
+    plt.annotate(xy = (3, 20), text = 'value', fontsize = fontsize, ha = 'right', fontweight = 'bold')
+
+    for i,colname in enumerate(nonzip):
+        plt.annotate(xy = (0, 19 - i), text = colname + ':', fontsize = fontsize, ha = 'right')
+        plt.annotate(xy = (0.5, 19-i), text = sample[colname], fontsize = fontsize, ha = 'right')
+        plt.annotate(xy = (1.5, 19-i), text = "{:,.0f}".format(round(linreg[-1].coef_[i], 0)), fontsize = fontsize, ha = 'right')
+        plt.annotate(xy = (3, 19-i), text = "{:,.0f}".format(round(sample[colname] * linreg[-1].coef_[i], 0)), fontsize = fontsize, ha = 'right')
+        
+        plt.plot([-1,3], [19 - i - 0.2, 19 - i - 0.2], linewidth = 0.5, color = 'grey')
+
+
+    plt.annotate(xy = (0, 2), text = 'zipcode:', fontsize = fontsize, ha = 'right')
+    plt.annotate(xy = (0.5, 2), text = find_zip(sample, zips), fontsize = fontsize, ha = 'right')
+    plt.annotate(xy = (1.5, 2), text = "{:,.0f}".format(round(get_zip_coef(sample, zips, linreg[-1].coef_), 0)), fontsize = fontsize, ha = 'right')
+    plt.annotate(xy = (3, 2), text = "{:,.0f}".format(round(get_zip_coef(sample, zips, linreg[-1].coef_), 0)), fontsize = fontsize, ha = 'right')
+    plt.plot([-1,3], [2 - 0.2, 2 - 0.2], linewidth = 0.5, color = 'grey')
+
+    plt.annotate(xy = (0, 1), text = 'intercept:', fontsize = fontsize, ha = 'right')
+    plt.annotate(xy = (3, 1), text = "{:,.0f}".format(round(linreg[-1].intercept_, 0)), fontsize = fontsize, ha = 'right')
+
+    plt.annotate(xy = (1.5, 0), text = 'Prediction:', fontsize = fontsize, ha = 'right', fontweight = 'bold',
+                color = 'red')
+
+    plt.plot([2,3], [0.9,0.9], linewidth = 3, color = 'black')
+
+    plt.annotate(xy = (3, 0), text = "${:,.0f}".format(round(linreg.predict(sample.values.reshape(1,-1))[0], 0)), fontsize = fontsize, ha = 'right', fontweight = 'bold',
+                color = 'red')
+
+    plt.axis('off');
